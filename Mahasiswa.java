39369183390a685978d76c1e82d17ef4eca6b1c5
aditmk1 <73748815+aditmk1@users.noreply.github.com>
2020-11-07 21:35:55 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119075.latihan37;
+
+/**
+ *
+ * @author
+ * NAMA     : aditiya musthafa kamil
+ * KELAS    : IF-1
+ * NIM      : 10119002
+ * Deskripsi Program : Class untuk objek Mahasiswa
+ * 
+ * 
+ */
+public class Mahasiswa {
+    public Integer nim, nilai;
+}
